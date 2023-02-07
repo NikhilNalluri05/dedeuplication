ff7e7a2997fea8f436a2eee1c16a7e85447930ea
NsNa1 <120901413+NsNa1@users.noreply.github.com>
2023-02-07 16:10:37 +0530

    Add files via upload

---
+++ deduplication.sql
@@ -0,0 +1,57 @@
+/*
+SQLyog - Free MySQL GUI v5.13
+Host - 5.0.22-community-nt : Database - debuplication
+*********************************************************************
+Server version : 5.0.22-community-nt
+*/
+
+SET NAMES utf8;
+
+SET SQL_MODE='';
+
+create database if not exists `debuplication`;
+
+USE `debuplication`;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `username` varchar(30) default NULL,
+  `password` varchar(30) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `admin` */
+
+insert into `admin` (`username`,`password`) values ('deva','deva');
+
+/*Table structure for table `ownerprofile` */
+
+DROP TABLE IF EXISTS `ownerprofile`;
+
+CREATE TABLE `ownerprofile` (
+  `UserName` varchar(35) default NULL,
+  `filename` varchar(700) NOT NULL,
+  `checksum` varchar(100) NOT NULL default '',
+  PRIMARY KEY  (`checksum`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ownerprofile` */
+
+/*Table structure for table `registration` */
+
+DROP TABLE IF EXISTS `registration`;
+
+CREATE TABLE `registration` (
+  `username` varchar(30) default NULL,
+  `password` varchar(30) default NULL,
+  `email` varchar(50) default NULL,
+  `phoneno` varchar(50) default NULL,
+  `userproductkey` varchar(50) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `registration` */
+
+SET SQL_MODE=@OLD_SQL_MODE;
